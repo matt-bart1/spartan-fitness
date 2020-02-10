01ab6ae29d4dbf5407cd62608986b8749232ea74
matt barton <mattbartondesigns@yahoo.co.uk>
2020-02-10 18:37:21 +0000

    first

---
+++ app.js
@@ -0,0 +1,13 @@
+
+    const menuToggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('nav');
+    menuToggle.addEventListener('click', () => {
+      if (nav.className != 'active') {
+        nav.className = 'active';
+
+      } else {
+        nav.className = '';
+
+      }
+
+    });
